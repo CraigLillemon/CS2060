630462662b1046e14df9e8f9db1edc29cc5f21ad
Trollattack99 <craig.lillemon@gmail.com>
2023-02-24 19:19:35 -0700

    Update LillemoCraigHW04.c

--- CS2060-Class-Code-main/CS2060-Class-Code-main/examples/Homework/LillemoCraigHW04.c
+++ CS2060-Class-Code-main/CS2060-Class-Code-main/examples/Homework/LillemoCraigHW04.c
@@ -1,12 +1,22 @@
-// Fig. 6.22: fig06_22.c
-// Two-dimensional array manipulations.
+/*
+* Craig Lillemon 2/24/2023
+* Due 2/24/2023
+* HW04 
+* Using multi Dimisonal arrays take inputs from multiple students in which each student contains multiple grade category assignments, get valid input
+* Display the grades, then use grade categories which is an array to calculate the final grade, then store the final grades in a 1d array
+* Caluclate and display class average
+* Round all numbers that display to the nearest .1
+*/
 #include <stdio.h>
-#include <math.h>
 #define STUDENTS 4
 #define GRADE_CATEGORIES 5
 #define CATEGORIES "1. Learning Activity 2. Homework 3. Project 4. Midterm 5. Final "
-const unsigned int MIN_GRADE = 0;
-const unsigned int MAX_GRADE = 105;
+//if want to change the weights in acordance with each cateogory they follow the trend above
+const double GRADE_CATEGORY_WEIGHT[] = { 0.1, 0.3, 0.3, 0.15, .15 };
+//Tried using const unsigned ints, to create a min max value for valid grade input
+//got warnings, just curious to know how can I change that
+#define MIN_GRADE  0
+#define MAX_GRADE 105
 //Gets a if >= 90
 const double  A_LETTER = 90;
 //Gets B if >= 80 while being less than A letter
@@ -14,41 +24,55 @@ const double  B_LETTER = 80;
 //Repeats like above
 const double  C_LETTER = 70;
 const double  D_LETTER = 60;
-const double GRADE_CATEGORY_WEIGHT[] = { 0.1, 0.3, 0.3, 0.15, .15 };
+//function prototypes
+//Personal note for Craig (me), it may say prototype, does not mean optinal, always include.
 double getGPA(const int setOfGrades[], size_t tests);
 void enterGrades( int grades[][GRADE_CATEGORIES], size_t pupils, size_t tests);
 char getLetter( double finalGrades);
+double getClassAverage(const double grades);
 void printArray(const int grades[][GRADE_CATEGORIES], size_t pupils, size_t tests);
 // function main begins program execution
 int main(void)
 
 {
+    //Initalize to restrict
+    double classAverage = 0;
+    double finalGrade[GRADE_CATEGORIES] = { 0,0,0,0,0 };
+    int studentGrades[STUDENTS][GRADE_CATEGORIES];
+    //Prints information
     printf("%s\n", CATEGORIES);
+    //Prints weights with acording information
     for (size_t activites = 0; activites < GRADE_CATEGORIES; ++activites) {
         printf("Category %d is %.2lf\n", (activites+1), GRADE_CATEGORY_WEIGHT[activites]);
     }
-    double classAverage = 0;
-    double finalGrade[GRADE_CATEGORIES] = { 0,0,0,0,0 };
-    // initialize student grades for three students (rows)
-    
-    int studentGrades[STUDENTS][GRADE_CATEGORIES];
+    //Input grades, from 0 105, can bechanged as it is const on top
     enterGrades(studentGrades, STUDENTS, GRADE_CATEGORIES);
    
     
     // output array studentGrades
     printArray(studentGrades, STUDENTS, GRADE_CATEGORIES);
-
-    // calculate average grade for each student
+  
+   
+    // calculate final grade for each student
     for (size_t student = 0; student < STUDENTS; ++student) {
+        //Determines the weight of the grades, set the information recieved to an 1d array to store and display
         finalGrade[student] = getGPA(studentGrades[student], GRADE_CATEGORIES);
         printf("\nStudent %u:  %.1f  %c\n",
             (student + 1), finalGrade[student], getLetter(finalGrade[student]));
+        //Sums up final grades to calculate average easy
         classAverage += finalGrade[student];
     }
-    printf("%s %.1lf", "The class Average is", (classAverage / STUDENTS));
+    //Prints class average
+    printf("%s %.1lf", "The class Average is", getClassAverage(classAverage));
 }
 
-// Print the array
+// Determines the total class average final grades after weight
+double getClassAverage(const double grades)
+{
+    double total = grades / STUDENTS;
+    return total;
+}
+//Prints array according to student input grades
 void printArray(const int grades[][GRADE_CATEGORIES], size_t pupils, size_t tests)
 {
     // output column heads
@@ -65,24 +89,29 @@ void printArray(const int grades[][GRADE_CATEGORIES], size_t pupils, size_t test
         }
     }
 }
+//Takes input from multi dimsional array, with studenty and grades categories as the sizes, 
 void enterGrades(int grades[][GRADE_CATEGORIES], size_t pupils, size_t tests)
 {
     printf("%s\n", CATEGORIES);
+    //Student for loop 
     for (size_t row = 0; row < pupils; row++)
     {
        
+        //Grade category for loop
         for (size_t column = 0; column < tests; column++)
         {
+            //Ensure that grades are valid, uses a do while loop to ensure, if not clears buffer and restarts on current array, to get proper input
             do 
             {
                 printf("\nEnter grade for each category  for student [%d], category [%d]:", (row+1), (column+1));
                 scanf("%d", &grades[row][column]);
             } 
-            while (grades[row][column] < MIN_GRADE || grades[row][column]> MAX_GRADE);
+            while ((grades[row][column] < MIN_GRADE) || (grades[row][column] > MAX_GRADE));
         }
 
     }
 }
+//Determines the total final grade, after weights
 double getGPA(const int setOfGrades[], size_t tests)
 {
     double total = 0;
@@ -92,7 +121,7 @@ double getGPA(const int setOfGrades[], size_t tests)
     }
     return total;
 }
-
+//Ensure that final grade asscocaties with proper, easily interchangable ontop as it is a const.
 char getLetter(const double finalGrades)
 {
     char letter = 'F';
