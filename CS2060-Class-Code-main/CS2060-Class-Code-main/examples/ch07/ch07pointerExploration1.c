a78401be76c65f1fc08dd0607bfc9cc8ebb4eb35
Trollattack99 <craig.lillemon@gmail.com>
2023-03-06 02:06:39 -0700

    xzS
    xz

--- CS2060-Class-Code-main/CS2060-Class-Code-main/examples/ch07/ch07pointerExploration1.c
+++ CS2060-Class-Code-main/CS2060-Class-Code-main/examples/ch07/ch07pointerExploration1.c
@@ -13,26 +13,28 @@ int main ()
 	int houseNum = 13;
 	int calcHouseNum1 = 0;
 	int calcHouseNum2 = 0;
-	int *houseNumPtr = &houseNum;
-	
+	int *houseNumPtr = NULL;
+	houseNumPtr = &houseNum;
+	//Prints the value stored
 	printf("houseNum %d\n", houseNum);
-
+	//Prints the address of the houseNum
 	printf("&houseNum %p\n\n", &houseNum);
-
+	//Points to the value stored in address 
+	//Prints the value stored in the address of the pointer, which holds the address of house num 
 	printf ("*houseNumPtr  %d\n", *houseNumPtr);
-
+	//Prints the address of houseNum
 	printf ("houseNumPtr %p\n\n", houseNumPtr);
-
+	//Prints the address of houseNumPtr which inside the address is the address of the housenum, which holds the values 
 	printf ("&houseNumPtr %p\n\n", &houseNumPtr);
-
+	//Points to the values stored in the address, which is 13 for both
 	calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
-
+	//Prints the value stored
 	printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
-
+	//Says to 2 times the pointer value stored at the address 
 	calcHouseNum2 = 2 * (*houseNumPtr);
-
+	//Prints the value stored
 	printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
-
+	//Prints the address stored within houseNumPtr
 	printf("\n&*houseNumPtr = %p"
 		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
 			
--- CS2060-Class-Code-main/CS2060-Class-Code-main/examples/ch07/ch07sec7_4CubePassRef.c
+++ CS2060-Class-Code-main/CS2060-Class-Code-main/examples/ch07/ch07sec7_4CubePassRef.c
@@ -5,18 +5,24 @@
 #include <stdlib.h>
 
 int cubePass1(int number);
-int cubePass2(int * numberPtr);
+int cubePass2(int* numberPtr);
 
 int main(void) {
 
 	int numberMain = 5;
 	int result = 0;
-	
+	//Prints value
 	printf ("In main before cubePass1  numberMain = %d\n", numberMain);
+	//Prints address
 	printf("&numberMain = %p\n", &numberMain);
+	//Sets result = to the function of cubepass1 with the argument being number main, which holds the value of 5
+	//Cubepass1 prints all according information
+	//Sets result = to return value which is 125
 	result = cubePass1(numberMain);
+
 	printf ("In main after cubePass1  numberMain = %d\n", numberMain);
 	printf ("Result = %d\n", result);
+
 	printf("\nIn main before cubePass2  numberMain = %d\n", numberMain);
 	result = cubePass2(&numberMain);
 	printf("\nIn main after cubePass2  numberMain = %d\n", numberMain);
@@ -24,19 +30,33 @@ int main(void) {
 
 } // main
 
-
+//Parameter will become 5
 int cubePass1 (int number) 
 {
 	int cube = 0;
 	puts("\nIn cubePass1");
+	//Value
 	printf("number = %d\n", number);
+	//Address
 	printf("&number = %p\n", &number);
+	//value to return
 	cube = number * number * number;
 	printf("cube  = %d\n", cube);
+	//Both end up as 125
 	number = cube;
 	printf("number = %d\n", number);
 	return cube;
 } 
+/*
+void cubePass2(const int* const numberPtr) {
+	printf("   *numberPtr = %d\n", *numberPtr);
+	printf("   numberPtr = %p\n", numberPtr);
+	int cube = (*numberPtr) * (*numberPtr) * (*numberPtr);
+	*numberPtr = cube;
+	numberPtr = &cube;
+	printf("   *numberPtr = %d\n\n", *numberPtr);
+}
+*/
 
 int cubePass2 (int * numberPtr) 
 {
